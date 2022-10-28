9e1ee4675e1bb2433b067ea50ba46abda1496167
jimartinez <jimartinez@theworkshop.com>
2022-10-28 16:35:15 +0200

    add elastic APM to hello world front end

--- README.md
+++ README.md
@@ -12,11 +12,20 @@ cd backend1
 mvn spring-boot:run
 ```
 
-### Backend 1
+### Backend 2
 ```
 cd backend2
 mvn spring-boot:run
 ```
 
+### Frontent
+```
+cd frontend/hello-world
+ng serve --open
+```
+
 # Ref:
 - https://levelup.gitconnected.com/how-to-integrate-elastic-apm-java-agent-with-spring-boot-7ce8388a206e
+- https://www.elastic.co/guide/en/elasticsearch/reference/current/docker.html
+- https://www.elastic.co/guide/en/apm/agent/rum-js/current/angular-integration.html
+- https://www.elastic.co/guide/en/apm/guide/current/configuration-rum.html
--- frontend/hello-world/src/app/app.module.ts
+++ frontend/hello-world/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ApmModule, ApmService } from '@elastic/apm-rum-angular'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,8 +16,21 @@ import { HelloWorldComponent } from './hello-world/hello-world.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ApmModule
   ],
-  providers: [],
+  providers: [ApmService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(service: ApmService) {
+    // Agent API is exposed through this apm instance
+    const apm = service.init({
+      serviceName: 'frontend',
+      serverUrl: 'http://localhost:8200',
+      environment: 'dev',
+      logLevel: 'trace',
+    })
+  }
+
+
+}
