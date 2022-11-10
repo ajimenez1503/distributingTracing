d31bdcdb87e6e787fb2722576d42336958312885
jimartinez <jimartinez@theworkshop.com>
2022-11-10 08:36:21 +0100

    OpenTelemetry Angular Interceptor

--- frontend/hello-world/src/app/app.module.ts
+++ frontend/hello-world/src/app/app.module.ts
@@ -5,6 +5,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import {
+  OpenTelemetryInterceptorModule,
+  CompositePropagatorModule,
+  OtelColExporterModule,
+} from '@jufab/opentelemetry-angular-interceptor';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,22 @@ import { HelloWorldComponent } from './hello-world/hello-world.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    OpenTelemetryInterceptorModule.forRoot({
+      commonConfig: {
+        console: true, // Display trace on console (only in DEV env)
+        production: false, // Send Trace with BatchSpanProcessor (true) or SimpleSpanProcessor (false)
+        serviceName: 'frontend', // Service name send in trace
+        probabilitySampler: '1',
+      },
+      otelcolConfig: {
+        url: 'http://127.0.0.1:4318/v1/traces', // URL of opentelemetry collector
+      },
+    }),
+    //Insert OtelCol exporter module
+    OtelColExporterModule,
+    //Insert propagator module
+    CompositePropagatorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
